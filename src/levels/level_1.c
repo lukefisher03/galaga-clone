6194ced83eb24f8052e8f584938ec190bf521c4d
Luke Fisher <lukedavidfisher03@gmail.com>
2025-01-21 23:35:02 -0500

    tested it out with 2 clusters

--- src/levels/level_1.c
+++ src/levels/level_1.c
@@ -12,7 +12,7 @@ struct Level *build_level_1(SDL_Renderer *renderer) {
         return NULL;
     }
 
-    level->num_clusters = 1;
+    level->num_clusters = 2;
     printf("num clusters: %i\n", level->num_clusters);
     level->enemy_clusters =
         calloc(level->num_clusters, sizeof(struct EnemyCluster));
@@ -29,10 +29,18 @@ struct Level *build_level_1(SDL_Renderer *renderer) {
         cluster->enemies = calloc(5, sizeof(struct Enemy));
         cluster->texture = load_bmp_texture("assets/enemy.bmp", renderer);
         cluster->size = 5;
-        for (size_t j = 0; j < cluster->size; ++j) {
-            initialize_default_enemy(&cluster->enemies[j],
-                                     550 + ((SHIP_SIZE + 50) * j), 200);
-        }
+    }
+
+    struct EnemyCluster *cluster1 = &level->enemy_clusters[0];
+    for (size_t j = 0; j < cluster1->size; ++j) {
+        initialize_default_enemy(&cluster1->enemies[j],
+                                 300 + ((SHIP_SIZE + 50) * j), 200);
+    }
+
+    struct EnemyCluster *cluster2 = &level->enemy_clusters[1];
+    for (size_t j = 0; j < cluster2->size; ++j) {
+        initialize_default_enemy(&cluster2->enemies[j],
+                                 700 + ((SHIP_SIZE + 50) * j), 400);
     }
 
     return level;
